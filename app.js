165b3d22c018d33bd866074266f2d51622089ad8
Shivd131 <shivdeshpande131@gmail.com>
2024-05-30 18:03:05 +0530

    feat: reading data from the file

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ const fs = require("fs/promises");
 //writing callback is more performant and preferred, however with promises, we avoid nested callbacks/ callback hell. we can handel our async opns more gracefully.
 
 (async () => {
+  const commandFileHandler = await fs.open("./command.txt", "r");
+
   const watcher = fs.watch("./command.txt");
   for await (const event of watcher) {
     if (event.eventType === "change") {
       //the file was changed.
       console.log("the file was changed", event);
+      //next step, reading the content
+
+      //getting the size of the file:
+      const { size } = await commandFileHandler.stat();
+      //console.log(size);
+      const buff = Buffer.alloc(size);
+      const offset = 0; //loc of buffer from which we start filling
+      const length = buff.byteLength; //no of bytes to read
+      const position = 0; // location of reading data
+      const content = await commandFileHandler.read(
+        buff,
+        offset,
+        length,
+        position
+      );
+      console.log(content);
     }
   }
 })();
