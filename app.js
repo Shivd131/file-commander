b983bc1a2629043eb859072685808d4fd7c93926
Shivd131 <shivdeshpande131@gmail.com>
2024-05-30 18:12:38 +0530

    feat: cleanup code using event emitter methods

--- app.js
+++ app.js
@@ -5,27 +5,33 @@ const fs = require("fs/promises");
 (async () => {
   const commandFileHandler = await fs.open("./command.txt", "r");
 
+  //as the next step, as all the FileHandle objects are <EventEmitter>s. using the emit property, we trigger it from line 34
+  commandFileHandler.on("change", async () => {
+    //the file was changed.
+    console.log("the file was changed");
+    //next step, reading the content
+
+    //getting the size of the file:
+    const { size } = await commandFileHandler.stat();
+    //console.log(size);
+    //allocating buffer with the size of our file.
+    const buff = Buffer.alloc(size);
+    //params for .read()
+    const offset = 0; //loc of buffer from which we start filling
+    const length = buff.byteLength; //no of bytes to read
+    const position = 0; // location of reading data
+    const content = await commandFileHandler.read(
+      buff,
+      offset,
+      length,
+      position
+    );
+    console.log(content);
+  });
   const watcher = fs.watch("./command.txt");
   for await (const event of watcher) {
     if (event.eventType === "change") {
-      //the file was changed.
-      console.log("the file was changed", event);
-      //next step, reading the content
-
-      //getting the size of the file:
-      const { size } = await commandFileHandler.stat();
-      //console.log(size);
-      const buff = Buffer.alloc(size);
-      const offset = 0; //loc of buffer from which we start filling
-      const length = buff.byteLength; //no of bytes to read
-      const position = 0; // location of reading data
-      const content = await commandFileHandler.read(
-        buff,
-        offset,
-        length,
-        position
-      );
-      console.log(content);
+      commandFileHandler.emit("change");
     }
   }
 })();
